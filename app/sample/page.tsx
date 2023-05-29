d7f2d7c1367b7b01a959e3bb7137ce0bd449d123
kanto <watanabe.kanto@gmail.com>
2023-05-29 15:12:43 +0900

    クエリパラメータの取得

--- app/sample/page.tsx
+++ app/sample/page.tsx
@@ -1,12 +1,19 @@
+"use client"
+
 import styles from '../page.module.css'
 import Link from 'next/link'
+import { useSearchParams } from 'next/navigation';
 
 export default function Home() {
+  // https://nextjs.org/docs/app/api-reference/functions/use-search-params
+  const searchParams = useSearchParams();
+  const search = searchParams.get('query');
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          This is Sample Page.
+          This is Sample Page.  {search}
         </p>
         <Link href="/sample/sample1-2">Go to Sample1-2.</Link>
         <Link href="/sample/111">Go to Sample Detail.</Link>
