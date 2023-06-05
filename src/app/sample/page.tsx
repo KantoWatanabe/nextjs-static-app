094e681b26b6cfd01e1477ac313cb0c478c0a16a
kanto <watanabe.kanto@gmail.com>
2023-06-05 23:31:54 +0900

    srcディレクトリに移動

--- src/app/sample/page.tsx
+++ src/app/sample/page.tsx
@@ -4,7 +4,7 @@ import styles from '../page.module.css'
 import Link from 'next/link'
 import { useSearchParams } from 'next/navigation';
 import useSWR from 'swr';
-import { basePath } from '../lib/util'
+import { basePath } from '../../lib/util'
 
 export default function Home() {
   // https://nextjs.org/docs/app/api-reference/functions/use-search-params
@@ -18,7 +18,7 @@ export default function Home() {
           This is Sample Page.  {search}
         </p>
         <Link href="/sample/sample1-2">Go to Sample1-2.</Link>
-        <Link href="/sample/111">Go to Sample Detail.</Link>
+        {/*<Link href="/sample/111">Go to Sample Detail.</Link>-->*/}
         <Link href="/">Back to Home.</Link>
         <TestList />
       </div>
