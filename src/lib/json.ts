baeefe36fe2f96b1656e391fe93053b6628c186a
kanto <watanabe.kanto@gmail.com>
2023-07-23 22:04:02 +0900

    swrの設定を追加

--- src/lib/json.ts
+++ src/lib/json.ts
@@ -1,16 +1,25 @@
 import useSWR from 'swr';
+import type { SWRConfiguration } from 'swr'
 import {TestJson} from '@/interfaces';
 import { basePath } from '@/lib/util';
 
 const BASE_URL = basePath();
+
+// https://swr.vercel.app/ja/docs/api#options
+let baseConfig: SWRConfiguration = {
+  errorRetryCount: 3,
+  revalidateOnFocus: false,
+  revalidateOnReconnect: false,
+  //revalidateIfStale: false,
+};
 
 const fetcher = async <T>(url: string): Promise<T> => {
   return fetch(url)
     .then((res) => res.json())
 };
 
-export function useTest() {
-  const swr = useSWR<TestJson>(`${BASE_URL}/json/test.json`, fetcher);
+export function useTest(config: SWRConfiguration = baseConfig) {
+  const swr = useSWR<TestJson>(`${BASE_URL}/json/test.json`, fetcher, config);
   return {
     ...swr,
     isLoading: !swr.error && !swr.data,
